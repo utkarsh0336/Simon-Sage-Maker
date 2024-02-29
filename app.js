22005de534790240a3010f3c636c6db99c2e8059
utkarsh0336 <110279358+utkarsh0336@users.noreply.github.com>
2024-02-29 11:47:02 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,92 @@
+let gameSeq = [];
+let userSeq = [];
+
+let btns = ["yellow","red","purple","green"];
+
+let started = false;
+let level = 0;
+let score = 0;
+
+let h2 = document.querySelector("h2");
+let h3 = document.querySelector("h3");
+
+document.addEventListener("keypress",function(){
+    if(started == false){
+    console.log("Game is Started");
+    started = true;
+    levelUp();
+    }
+});
+
+function gameFlash(btn){
+    btn.classList.add("flash");
+    setTimeout(function(){
+        btn.classList.remove("flash");
+    },250)
+}
+
+function userFlash(btn){
+    btn.classList.add("userflash");
+    setTimeout(function(){
+        btn.classList.remove("userflash");
+    },250)
+}
+
+function checkAns(idx){
+    // console.log("curr level : ",level);
+    if(userSeq[idx] == gameSeq[idx]){
+        if(userSeq.length == gameSeq.length){
+           setTimeout(levelUp,1000);
+        }
+    }
+    else{
+        h2.innerHTML = `Game Over ! Your Score was <b>${level}</b><br>Press any key to start.`;
+        document.querySelector("body").style.backgroundColor = "red";
+        setTimeout(function(){
+            document.querySelector("body").style.backgroundColor = "white";
+        },150)
+        if(score < level){
+            score = `${level}`;
+            h3.innerHTML = `<b> Your Maximum Score : ${score}`;
+        }
+        reset();
+    }
+}
+
+function btnPress(){
+    let btn = this;
+    userFlash(btn);
+    let userColor = btn.getAttribute("id");
+    userSeq.push(userColor);
+    console.log(userSeq);
+
+    checkAns(userSeq.length - 1);
+}
+
+function reset(){
+    started = false;
+    gameSeq = [];
+    userSeq = [];
+    level = 0;
+}
+
+let allBtns = document.querySelectorAll(".btn");
+for (btn of allBtns){
+    btn.addEventListener("click",btnPress );
+}
+
+function levelUp(){
+    userSeq = [];
+    level++;
+    h2.innerText = `Level ${level}`;
+    
+    //random btn choose
+    let randIdx = Math.floor(Math.random()*3);
+    let randColor = btns[randIdx];
+    let randBtn = document.querySelector(`.${randColor}`);
+    gameSeq.push(randColor);
+    console.log(gameSeq);
+    gameFlash(randBtn);
+}
+
+
